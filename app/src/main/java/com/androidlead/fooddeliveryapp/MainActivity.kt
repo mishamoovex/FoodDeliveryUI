4e348eee27627f330f8266469d117612cc684a76
Mykhailo Vasylenko <vasylemv@fuib.com>
2024-09-08 13:18:21 +0300

    Set up colors

--- app/src/main/java/com/androidlead/fooddeliveryapp/MainActivity.kt
+++ app/src/main/java/com/androidlead/fooddeliveryapp/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.androidlead.fooddeliveryapp.ui.theme.FoodDeliveryAppTheme
+import com.androidlead.fooddeliveryapp.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FoodDeliveryAppTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FoodDeliveryAppTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/androidlead/fooddeliveryapp/ui/theme/Color.kt
+++ app/src/main/java/com/androidlead/fooddeliveryapp/ui/theme/Color.kt
@@ -1,11 +1,57 @@
 package com.androidlead.fooddeliveryapp.ui.theme
 
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Pink = Color(0xFFF28482)
+val Green = Color(0xFF84A59D)
+val Yellow = Color(0xFFF7EDE2)
+val YellowLight = Color(0xFFFFFFF2)
+val Dark = Color(0xFF3D405B)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+data class AppColors(
+    val background: Color,
+    val onBackground: Color,
+    val surface: Color,
+    val onSurface: Color,
+    val secondarySurface: Color,
+    val onSecondarySurface: Color,
+    val regularSurface: Color,
+    val onRegularSurface: Color,
+    val actionSurface: Color,
+    val onActionSurface: Color,
+    val highlightSurface: Color,
+    val onHighlightSurface: Color
+)
+
+val LocalAppColors = staticCompositionLocalOf {
+    AppColors(
+        background = Color.Unspecified,
+        onBackground = Color.Unspecified,
+        surface = Color.Unspecified,
+        onSurface = Color.Unspecified,
+        secondarySurface = Color.Unspecified,
+        onSecondarySurface = Color.Unspecified,
+        regularSurface = Color.Unspecified,
+        onRegularSurface = Color.Unspecified,
+        actionSurface = Color.Unspecified,
+        onActionSurface = Color.Unspecified,
+        highlightSurface = Color.Unspecified,
+        onHighlightSurface = Color.Unspecified
+    )
+}
+
+val extendedColor = AppColors(
+    background = Color.White,
+    onBackground = Dark,
+    surface = Color.White,
+    onSurface = Dark,
+    secondarySurface = Pink,
+    onSecondarySurface = Color.White,
+    regularSurface = YellowLight,
+    onRegularSurface = Dark,
+    actionSurface = Yellow,
+    onActionSurface = Pink,
+    highlightSurface = Green,
+    onHighlightSurface = Color.White
+)
